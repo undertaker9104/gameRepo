abb10cc61141f218f88ca5c9efdfd9c89b807076
undertaker9104 <undertaker9104@gmail.com>
2019-11-13 15:02:03 +0800

    add upload picture to server and show picture to admin page

--- admin/src/views/ItemList.vue
+++ admin/src/views/ItemList.vue
@@ -2,14 +2,23 @@
   <div>
     <h1>物品列表</h1>
     <el-table :data="items">
-        <el-table-column prop="_id" label="ID" width="240"></el-table-column>
-        <el-table-column prop="name" label="物品名稱"></el-table-column>
-        <el-table-column fixed="right" label="操作" width="180">
+      <el-table-column prop="_id" label="ID" width="240"></el-table-column>
+      <el-table-column prop="name" label="物品名稱"></el-table-column>
+      <el-table-column prop="icon" label="圖片">
         <template slot-scope="scope">
-        <el-button type="text" size="small" @click="$router.push(`/items/edit/${scope.row._id}`)">編輯</el-button>
-        <el-button type="text" size="small" @click="remove(scope.row)">刪除</el-button>
-      </template>
-    </el-table-column>
+          <img :src="scope.row.icon" style="height:3rem;" />
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button
+            type="text"
+            size="small"
+            @click="$router.push(`/items/edit/${scope.row._id}`)"
+          >編輯</el-button>
+          <el-button type="text" size="small" @click="remove(scope.row)">刪除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -17,33 +26,33 @@
 
 <script>
 export default {
-    data() {
-        return {
-            items:[]
-        }
+  data() {
+    return {
+      items: []
+    };
+  },
+  methods: {
+    async fetch() {
+      const res = await this.$http.get("rest/items");
+      this.items = res.data;
     },
-    methods: {
-        async fetch(){
-            const res = await this.$http.get('rest/items')
-            this.items = res.data
-        },
-        async remove(row){
-            this.$confirm(`是否確定要刪除物品 "${row.name}"`, '提示', {
-          confirmButtonText: '確定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(async () => {
-            await this.$http.delete(`rest/items/${row._id}`)
-          this.$message({
-            type: 'success',
-            message: '刪除成功'
-          });
-          this.fetch()
-        })
-        }
-    },
-    created() {
+    async remove(row) {
+      this.$confirm(`是否確定要刪除物品 "${row.name}"`, "提示", {
+        confirmButtonText: "確定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(async () => {
+        await this.$http.delete(`rest/items/${row._id}`);
+        this.$message({
+          type: "success",
+          message: "刪除成功"
+        });
         this.fetch();
+      });
     }
-}
+  },
+  created() {
+    this.fetch();
+  }
+};
 </script>
