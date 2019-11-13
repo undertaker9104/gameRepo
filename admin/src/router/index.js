200e87befecdd8ed8a20d14cb7977fd585e6090a
Hsing Ying <32754177+undertaker9104@users.noreply.github.com>
2019-11-13 14:11:07 +0800

    fixed rest api to dynamic params and added Item pages (#2)

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router'
 import Main from '../views/Main.vue'
 import CategoryEdit from '../views/CategoryEdit.vue'
 import CategoryList from '../views/CategoryList.vue'
+import ItemEdit from '../views/ItemEdit.vue'
+import ItemList from '../views/ItemList.vue'
 
 Vue.use(VueRouter)
 
@@ -16,6 +18,10 @@ const routes = [
       {path: '/categories/create', component:CategoryEdit},
       {path: '/categories/list', component:CategoryList},
       {path: '/categories/edit/:id', component:CategoryEdit, props: true},
+
+      {path: '/items/create', component:ItemEdit},
+      {path: '/items/list', component:ItemList},
+      {path: '/items/edit/:id', component:ItemEdit, props: true},
     ]
   },
 ]
