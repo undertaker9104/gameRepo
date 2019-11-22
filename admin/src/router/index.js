4e24ce3f066456302045a7cbe63133282cc3431d
Hsing Ying <32754177+undertaker9104@users.noreply.github.com>
2019-11-22 22:21:07 +0800

    Feature/#11 add ads (#12)
    * add adds pages
    
    * fix adds word

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -10,6 +10,8 @@ import HeroEdit from '../views/HeroEdit.vue'
 import HeroList from '../views/HeroList.vue'
 import ArticleEdit from '../views/ArticleEdit.vue'
 import ArticleList from '../views/ArticleList.vue'
+import AdEdit from '../views/AdEdit.vue'
+import AdList from '../views/AdList.vue'
 
 Vue.use(VueRouter)
 
@@ -34,6 +36,11 @@ const routes = [
       { path: '/articles/create', component: ArticleEdit },
       { path: '/articles/list', component: ArticleList },
       { path: '/articles/edit/:id', component: ArticleEdit, props: true },
+
+
+      { path: '/ads/create', component: AdEdit },
+      { path: '/ads/list', component: AdList },
+      { path: '/ads/edit/:id', component: AdEdit, props: true },
     ]
   },
 ]
