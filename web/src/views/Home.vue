1fe45cddb6d5f3811810ad712ffd7a62477007ee
undertaker9104 <undertaker9104@gmail.com>
2019-11-25 11:22:38 +0800

    sass init

---
+++ web/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">首頁</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style>
+</style>
---
+++ web/src/views/Main.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <div>
+    <div class="topbar bg-black py-2 text-white">
+      topbar
+    </div>
+    <div class="nav">
+      nav
+    </div>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style >
+</style>
